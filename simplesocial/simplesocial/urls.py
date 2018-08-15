f12591518a667f8179b4339e1d64c2963814de1d
dawid <budzynski.dawid@gmail.com>
2018-08-15 21:41:57 +0200

    creating views, templates and url for groups

--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+
 from . import views
 
 urlpatterns = [
@@ -22,10 +23,12 @@ urlpatterns = [
     url(r'^$', views.HomePage.as_view(),
         name='home'),
     url(r'^accounts/', include('accounts.urls',
-                              namespace='accounts')),
+                               namespace='accounts')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^test/$', views.TestPage.as_view(),
         name='test'),
     url(r'^thanks/$', views.ThanksPage.as_view(),
-        name='thanks')
+        name='thanks'),
+    url(r'^groups/', include('groups.urls',
+                             namespace='groups')),
 ]
