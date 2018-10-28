2001ef79764d01933bbbf206fb13be2815da0ce3
dawid <budzynski.dawid@gmail.com>
2018-10-28 18:13:50 +0100

    changing folder structure

--- simplesocial/groups/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import (Form, CharField, Textarea, ImageField)
-
-
-class CreateGroupForm(Form):
-    name = CharField(max_length=64)
-    description = CharField(widget=Textarea, required=False)
-    image = ImageField(required=False)
--- simplesocial/tech_news/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TechNewsConfig(AppConfig):
-    name = 'tech_news'
